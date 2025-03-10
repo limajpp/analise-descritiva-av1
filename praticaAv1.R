b4edd7fa6a59e1a0a04fcde9644e3ed91b893031
João Pedro Lima <joaopedroca90@gmail.com>
2025-03-10 16:51:30 -0300

    feat: Added question 2 and padronized variable names

--- praticaAv1.R
+++ praticaAv1.R
@@ -35,11 +35,12 @@ tabelaRelativaDiaDaSemanaArredondada = round(tabelaRelativaDiaDaSemana, 4)
 tabelaSimplesDiaDaSemana
 tabelaRelativaDiaDaSemanaArredondada
 
+# Criando a tabela final de frequências da questão 1 e transformando em ".tex"
 library(xtable)
 
-tabela_completa <- data.frame(
+tabelaCompleta = data.frame(
   Variável = rep(c("Natureza", "Meio Empregado", "Gênero", 
-                   "Escolaridade da Vítima", "Raça da Vítima", "Dia da Semana"), 
+                   "Escolaridade da Vitima", "Raça da Vítima", "Dia da Semana"), 
                  times = c(length(tabelaSimplesNatureza),
                            length(tabelaSimplesMeioEmpregado),
                            length(tabelaSimplesGenero),
@@ -53,21 +54,63 @@ tabela_completa <- data.frame(
                 names(tabelaSimplesRacaDaVitima),
                 names(tabelaSimplesDiaDaSemana)),
   FrequênciaAbs = c(as.numeric(tabelaSimplesNatureza), 
-                 as.numeric(tabelaSimplesMeioEmpregado), 
-                 as.numeric(tabelaSimplesGenero),
-                 as.numeric(tabelaSimplesEscolaridadeDaVitima), 
-                 as.numeric(tabelaSimplesRacaDaVitima),
-                 as.numeric(tabelaSimplesDiaDaSemana)),
+                    as.numeric(tabelaSimplesMeioEmpregado), 
+                    as.numeric(tabelaSimplesGenero),
+                    as.numeric(tabelaSimplesEscolaridadeDaVitima), 
+                    as.numeric(tabelaSimplesRacaDaVitima),
+                    as.numeric(tabelaSimplesDiaDaSemana)),
   FrequênciaRel = c(as.numeric(tabelaRelativaNatureza), 
-                as.numeric(tabelaRelativaMeioEmpregado), 
-                as.numeric(tabelaRelativaGenero),
-                as.numeric(tabelaRelativaEscolaridadeDaVitima), 
-                as.numeric(tabelaRelativaRacaDaVitima),
-                as.numeric(tabelaRelativaDiaDaSemana))
+                    as.numeric(tabelaRelativaMeioEmpregado), 
+                    as.numeric(tabelaRelativaGenero),
+                    as.numeric(tabelaRelativaEscolaridadeDaVitima), 
+                    as.numeric(tabelaRelativaRacaDaVitima),
+                    as.numeric(tabelaRelativaDiaDaSemana))
 )
 
-tabela_completa$FrequênciaRel = round(tabela_completa$FrequênciaRel, 4)
+tabelaCompleta$FrequênciaRel = round(tabelaCompleta$FrequênciaRel, 4)
 
-print(xtable(tabela_completa, caption = "Tabela de Frequências e Proporções"),
-      include.rownames = FALSE, file = "C:/Users/joaop/OneDrive/Documentos/Metodos Quantitativos/Prática AV1/tabela_frequencias.tex")
+print(xtable(tabelaCompleta, caption = "Tabela de Frequências"),
+      include.rownames = FALSE, file = "C:/Users/joaop/OneDrive/Documentos/Metodos Quantitativos/Prática AV1/tabelaq1.tex")
 
+# Criando as tabelas cruzadas meioEmpregado vs. genero da questão 2
+tabelaMeioGenero = table(Base2025_1$`Meio Empregado`, Base2025_1$`Gênero`)
+tabelaMeioGeneroRelativa = prop.table(tabelaMeioGenero)
+tabelaMeioGeneroRelativaArredondada = round(tabelaMeioGeneroRelativa, 4)
+tabelaMeioGenero
+tabelaMeioGeneroRelativaArredondada
+
+# Criando as tabelas cruzadas escolaridadeDaVitima vs. racaDaVitima da questão 2
+tabelaEscolaridadeRaca = table(Base2025_1$`Escolaridade da Vítima`, Base2025_1$`Raça da Vítima`)
+tabelaEscolaridadeRacaRelativa = prop.table(tabelaEscolaridadeRaca)
+tabelaEscolaridadeRacaRelativaArredondada = round(tabelaEscolaridadeRacaRelativa, 4)
+tabelaEscolaridadeRaca
+tabelaEscolaridadeRacaRelativa
+tabelaEscolaridadeRacaRelativaArredondada
+
+# Convertendo tabelas para data.frames e mantendo apenas os nomes das categorias como primeira coluna(questão 2)
+dfMeioGenero <- as.data.frame.matrix(tabelaMeioGenero)
+dfMeioGenero <- cbind(rownames(dfMeioGenero), dfMeioGenero)
+colnames(dfMeioGenero)[1] <- "" 
+
+dfMeioGeneroRelativa <- as.data.frame.matrix(tabelaMeioGeneroRelativaArredondada)
+dfMeioGeneroRelativa <- cbind(rownames(dfMeioGeneroRelativa), dfMeioGeneroRelativa)
+colnames(dfMeioGeneroRelativa)[1] <- ""
+
+dfEscolaridadeRaca <- as.data.frame.matrix(tabelaEscolaridadeRaca)
+dfEscolaridadeRaca <- cbind(rownames(dfEscolaridadeRaca), dfEscolaridadeRaca)
+colnames(dfEscolaridadeRaca)[1] <- ""
+
+dfEscolaridadeRacaRelativa <- as.data.frame.matrix(tabelaEscolaridadeRacaRelativaArredondada)
+dfEscolaridadeRacaRelativa <- cbind(rownames(dfEscolaridadeRacaRelativa), dfEscolaridadeRacaRelativa)
+colnames(dfEscolaridadeRacaRelativa)[1] <- ""
+
+# Gerando as tabelas LaTeX corrigidas da questão 2
+print(xtable(dfMeioGenero, caption = "Tabela Cruzada: Meio Empregado vs Gênero"),
+      include.rownames = FALSE, file = "C:/Users/joaop/OneDrive/Documentos/Metodos Quantitativos/Prática AV1/tabelaMeioGeneroq2.tex")
+print(xtable(dfMeioGeneroRelativa, caption = "Tabela Cruzada Relativa: Meio Empregado vs Gênero"),
+      include.rownames = FALSE, file = "C:/Users/joaop/OneDrive/Documentos/Metodos Quantitativos/Prática AV1/tabelaMeioGeneroRelativaq2.tex")
+
+print(xtable(dfEscolaridadeRaca, caption = "Tabela Cruzada: Escolaridade da Vítima vs Raça da Vítima"),
+      include.rownames = FALSE, file = "C:/Users/joaop/OneDrive/Documentos/Metodos Quantitativos/Prática AV1/tabelaEscolaridadeRacaq2.tex")
+print(xtable(dfEscolaridadeRacaRelativa, caption = "Tabela Cruzada Relativa: Escolaridade da Vítima vs Raça da Vítima"),
+      include.rownames = FALSE, file = "C:/Users/joaop/OneDrive/Documentos/Metodos Quantitativos/Prática AV1/tabelaEscolaridadeRacaRelativaq2.tex")
