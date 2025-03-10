7785eba9328ff2d3387dea34926ea2d99c043249
João Pedro Lima <joaopedroca90@gmail.com>
2025-03-10 11:16:20 -0300

    Finished simple and relative frequencies for question

--- praticaAv1.R
+++ praticaAv1.R
@@ -1,36 +1,36 @@
-# Criando a estrutura de dados
-data.frame(Base2025_1)
-# Anexando ao ambiente
-attach(Base2025_1)
-
 # Criando as tabelas de frequência simples e relativas da questão 1:
 tabelaSimplesNatureza = table(Base2025_1$Natureza)
 tabelaRelativaNatureza = prop.table(tabelaSimplesNatureza)
 tabelaRelativaNaturezaArredondada = round(tabelaRelativaNatureza, 4)
 tabelaSimplesNatureza
-tabelaRelativaNatureza
 tabelaRelativaNaturezaArredondada
 
 tabelaSimplesMeioEmpregado = table(Base2025_1$`Meio Empregado`)
 tabelaRelativaMeioEmpregado = prop.table(tabelaSimplesMeioEmpregado)
 tabelaRelativaMeioEmpregadoArredondada = round(tabelaRelativaMeioEmpregado, 4)
 tabelaSimplesMeioEmpregado
-tabelaRelativaMeioEmpregado
 tabelaRelativaMeioEmpregadoArredondada
 
 tabelaSimplesGenero = table(Base2025_1$Gênero)
 tabelaRelativaGenero = prop.table(tabelaSimplesGenero)
 tabelaRelativaGeneroArredondada = round(tabelaRelativaGenero, 4)
 tabelaSimplesGenero
-tabelaRelativaGenero
 tabelaRelativaGeneroArredondada
 
-# TODO *Coluna "Alfabetizado está com contagem errada"*
 tabelaSimplesEscolaridadeDaVitima = table(Base2025_1$`Escolaridade da Vítima`)
+tabelaRelativaEscolaridadeDaVitima = prop.table(tabelaSimplesEscolaridadeDaVitima)
+tabelaRelativaEscolaridadeDaVitimaArredondada = round(tabelaRelativaEscolaridadeDaVitima, 4)
 tabelaSimplesEscolaridadeDaVitima
+tabelaRelativaEscolaridadeDaVitimaArredondada
 
 tabelaSimplesRacaDaVitima = table(Base2025_1$`Raça da Vítima`)
+tabelaRelativaRacaDaVitima = prop.table(tabelaSimplesRacaDaVitima)
+tabelaRelativaRacaDaVitimaArredondada = round(tabelaRelativaRacaDaVitima, 4)
 tabelaSimplesRacaDaVitima
+tabelaRelativaRacaDaVitimaArredondada
 
 tabelaSimplesDiaDaSemana = table(Base2025_1$`Dia da Semana`)
+tabelaRelativaDiaDaSemana = prop.table(tabelaSimplesDiaDaSemana)
+tabelaRelativaDiaDaSemanaArredondada = round(tabelaRelativaDiaDaSemana, 4)
 tabelaSimplesDiaDaSemana
+tabelaRelativaDiaDaSemanaArredondada
